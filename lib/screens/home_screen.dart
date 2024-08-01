d1e2b7ce12861ecf2f85a057b9fae8440a619883
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-08-01 17:12:22 +0600

    update app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_ui/styles/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: AppColors.backgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              //   appbar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //   menu
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: AppColors.lightBackgroundColor,
+                      borderRadius: BorderRadius.circular(13)
+                    ),
+                    child: Center(child: Icon(Icons.sort)),
+                  ),
+                  //   title
+                  //   profile image
+                  Container(
+                    height: 40,
+                    width: 30,
+                    decoration: BoxDecoration(
+                        color: AppColors.lightBackgroundColor,
+                        borderRadius: BorderRadius.circular(13),
+                        image: DecorationImage(image: NetworkImage('https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg'), fit: BoxFit.cover)
+                    ),
+                  ),
+                ],
+              )
+              //   title
+              //   searchbar
+              //   sliders
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
