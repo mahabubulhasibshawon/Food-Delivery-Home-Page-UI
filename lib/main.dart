fd7a7248ccfdc9c72fbbcc3b0a90525f7070580f
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-08-01 16:50:04 +0600

    add AppColors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xffE76435);
+  static const backgroundColor = Color(0xff292A37);
+  static const lightBackgroundColor = Color(0xff32323F);
+  static const whiteColor = Color(0xffFEFEFE);
+}
