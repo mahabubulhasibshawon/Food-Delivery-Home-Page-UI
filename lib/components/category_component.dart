0e3809dd9e19f59a0745b398f80a57a4577c3d74
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-08-02 12:22:11 +0600

    test category widgets

--- lib/components/category_component.dart
+++ lib/components/category_component.dart
@@ -8,11 +8,17 @@ class CategoryComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    final categories = [
+      {'imageUrl': 'https://static.vecteezy.com/system/resources/thumbnails/027/141/485/small_2x/pizza-created-with-generative-ai-png.png', 'foodName': 'Pizza'},
+      {'imageUrl': 'https://t4.ftcdn.net/jpg/05/65/22/05/360_F_565220597_DjMSnmmbovq13MB8b8ET8ydwP0iOKuMX.jpg', 'foodName': 'Burger'},
+      {'imageUrl': 'https://media.post.rvohealth.io/wp-content/uploads/2021/09/sushi-sashimi-732x549-thumbnail-732x549.jpg', 'foodName': 'Sushi'},
+      {'imageUrl': 'https://images.immediate.co.uk/production/volatile/sites/30/2021/04/Pasta-alla-vodka-f1d2e1c.jpg', 'foodName': 'Pasta'},
+    ];
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(
+        const Text(
           'Fastest Delivery',
           style: TextStyle(
             fontWeight: FontWeight.bold,
@@ -21,14 +27,19 @@ class CategoryComponent extends StatelessWidget {
           ),
         ),
       //   slides
-        Row(
-          children: [
-            CategoryItemWidget(),
-            CategoryItemWidget(),
-            CategoryItemWidget(),
-            CategoryItemWidget(),
-          ],
-        )
+        SizedBox(
+          height: 100, // Set an appropriate height for the ListView
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 4, // Set the number of items
+            itemBuilder: (context, index) {
+              return CategoryItemWidget(
+                imageUrl: categories[index]['imageUrl']!,
+                foodName: categories[index]['foodName']!,
+              );
+            },
+          ),
+        ),
       ],
     );
   }
--- lib/widgets/category_item_widget.dart
+++ lib/widgets/category_item_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CategoryItemWidget extends StatelessWidget {
-  const CategoryItemWidget({super.key});
+  const CategoryItemWidget({super.key, required this.imageUrl, required this.foodName});
+  final String imageUrl;
+  final String foodName;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +16,26 @@ class CategoryItemWidget extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.amber,
           borderRadius: BorderRadius.circular(40)
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Image.network(
+                imageUrl,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 8),
+              Text(
+                foodName,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
